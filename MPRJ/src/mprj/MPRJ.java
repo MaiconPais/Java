1105d356d13dcfcc9640f5d270183240b036d041
yago1 <yagof_costa@hotmail.com>
2019-07-10 23:34:53 -0300

    update mainForm

--- MPRJ/src/mprj/MPRJ.java
+++ MPRJ/src/mprj/MPRJ.java
@@ -2,25 +2,14 @@
 package mprj;
 
 public class MPRJ {
-    
-     String nome;
-     String perfil;
-     double matricula;
-     String nomeCraai;
-     String commarca;
-     String Orgao;
-     String Endereço;
-     String tel;
-     
-     
-     public void ObterDados(){
-         
-     }
      
     public static void main(String[] args) {
         
-        Cadastro cadatro =  new Cadastro();
-        cadatro.setVisible(true);
+        //Cadastro cadatro =  new Cadastro();
+        //cadatro.setVisible(true);
+        
+        MainForm main = new MainForm();
+        main.setVisible(true);
         
     }
      
